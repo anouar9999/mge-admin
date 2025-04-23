affb419ac798493ef3b4a3d3556a08969abcf00a
anouar9999 <sabiranouar99@gmail.com>
2025-04-23 04:15:14 +0000

    commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RHwsyhZ/geDNpSiICQy5w1cFFuDrfRAltcGwyYzUvAo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"PrVkmSpn9Jealru+nYxwbWYYZ4r4lKetkUVlJvsUNv8=\"}"
